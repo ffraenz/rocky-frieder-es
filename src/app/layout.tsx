3eba7c748e4a5a0efdfc69026d37904ac6206bf6
Fränz Friederes <fraenz@frieder.es>
2024-03-12 12:23:49 +0100

    Add Fathom Analytics web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { baseUrl, locale } from '@/consts'
+import { baseUrl, fathomAnalyticsSiteId, locale } from '@/consts'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,15 +25,22 @@ export default function RootLayout({
           <footer className="pt-6">
             <p>
               For Anni.
-              By <a href="https://fraenz.frieder.es" rel="noopener" className="underline">Fränz</a>.
+              By <a href="https://wierk.lu" rel="noopener" className="underline">Fränz</a>.
             </p>
             <p className="pt-1 text-gray-400">
-              Updated automatically.
-              View source code on <a href="https://github.com/ffraenz/rocky-frieder-es" rel="noopener" className="underline">GitHub</a>.
+              Updated automagically.
+              View <a href="https://github.com/ffraenz/rocky-frieder-es" rel="noopener" className="underline">source code</a> and <a href="https://app.usefathom.com/share/brcszhed/rocky.frieder.es" rel="noopener" className="underline">web analytics</a>.
               Paw icon created by <a href="https://thenounproject.com/icon/paw-print-6055246/" rel="noopener nofollow" className="underline">Oh Rian</a>.
             </p>
           </footer>
         </main>
+        {fathomAnalyticsSiteId !== undefined && (
+          <script
+            src="https://cdn.usefathom.com/script.js"
+            data-site={fathomAnalyticsSiteId}
+            defer
+          ></script>
+        )}
       </body>
     </html>
   )
